7cd3f2345da4cfee25b223d3c1d6470776b78e8f
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-22 03:27:52 +0530

    first commit

---
+++ src/com/linecompareclasses/LineComapre.java
@@ -0,0 +1,8 @@
+package com.linecompareclasses;
+
+public class LineComapre {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comparison computation program");
+        System.out.println("**********************************************");
+    }
+}
